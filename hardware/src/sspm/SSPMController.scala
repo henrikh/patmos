e59458dbb0f223256d554f8d4e3b2cf3014575f3
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2017-03-28 16:25:06 +0200

    SSPM Connector impl.

--- hardware/src/sspm/SSPMController.scala
+++ hardware/src/sspm/SSPMController.scala
@@ -27,7 +27,25 @@ class SSPMConnector extends IODevice() {
   // OCP pins and SSPMBackbone pins
   override val io = new IODeviceIO() with SSPMConnectorSignals
 
+  val respReg = Reg(init = OcpResp.NULL)
+  val cmdAcceptReg = Reg(init = Bits(width = 1))
+
+  respReg := OcpResp.NULL
+  cmdAcceptReg := Bits(0)
+
+  when(io.connectorSignals.enable === Bits(1)) {
+    when(io.ocp.M.Cmd === OcpCmd.RD || io.ocp.M.Cmd === OcpCmd.WR) {
+      respReg := OcpResp.DVA
+      cmdAcceptReg := Bits(1)
+    }
+  }
+
+  io.ocp.S.Resp := respReg
+  io.ocp.S.CmdAccept := cmdAcceptReg
+
   io.connectorSignals.M.Data := io.ocp.M.Data
+  io.connectorSignals.M.Addr := io.ocp.M.Addr
+  io.connectorSignals.M.ByteEn := io.ocp.M.ByteEn
   io.ocp.S.Data := io.connectorSignals.S.Data
 }
 
@@ -49,10 +67,12 @@ class SSPMConnectorTester(dut: SSPMConnector) extends Tester(dut) {
   poke(dut.io.ocp.M.Addr, 1)
   poke(dut.io.ocp.M.ByteEn, Bits("b1111").litValue())
 
+  poke(dut.io.connectorSignals.enable, 1)
+
   step(1)
 
   expect(dut.io.connectorSignals.M.ByteEn, Bits("b1111").litValue())
-  poke(dut.io.connectorSignals.M.Addr, 1)
+  expect(dut.io.connectorSignals.M.Addr, 1)
 
   poke(dut.io.connectorSignals.S.Data, 42)
 
