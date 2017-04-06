fb3a836917f7fb65d3dcd262d4b93c9e883fc590
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2017-04-06 09:01:26 +0200

    SSPM: Example usage of SSPM

---
+++ c/sspm.c
@@ -0,0 +1,35 @@
+/*
+    This is a minimal C program executed on the FPGA version of Patmos.
+    An embedded Hello World program: a blinking LED.
+
+    Additional to the blinking LED we write to the UART '0' and '1' (if available).
+
+    Author: Martin Schoeberl
+    Copyright: DTU, BSD License
+*/
+
+#include <machine/spm.h>
+#include <stdio.h>
+
+int main() {
+
+	volatile _SPM int *sspm_ptr = (volatile _SPM int *) 0xF0040000;
+	volatile _SPM int *uart_ptr = (volatile _SPM int *) 0xF0080004;
+	int i, j;
+  int k = 0;
+
+  *sspm_ptr = 0x42;
+
+	sspm_ptr += 1;
+
+	*sspm_ptr = 0x41;
+
+  printf("%x\n", (int) sspm_ptr);
+  printf("%x\n", (int) *sspm_ptr);
+
+	sspm_ptr -= 1;
+
+  printf("%x\n", (int) *sspm_ptr);
+
+  putchar('1');
+}
