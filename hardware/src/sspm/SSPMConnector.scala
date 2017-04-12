b4f835ca88ec209751b61b08ddb644ccea1c3b15
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2017-04-12 12:04:38 +0200

    SSPM: Connector stores all signals and responds when the results are valid

--- hardware/src/sspm/SSPMConnector.scala
+++ hardware/src/sspm/SSPMConnector.scala
@@ -31,22 +31,35 @@ class SSPMConnector extends CoreDevice() {
   val writeEnableReg = Reg(init = Bits(0, width = 1))
   val MAddrReg = Reg(init = Bits(width = ADDR_WIDTH))
   val MDataReg = Reg(init = Bits(width = DATA_WIDTH))
+  val MByteEnReg = Reg(init = Bits(width = 4))
+  val armedReg = Reg(init = Bool(false))
 
   respReg := OcpResp.NULL
   writeEnableReg := Bits(0)
   MAddrReg := Bits(0)
   MDataReg := Bits(0)
 
+  when(io.ocp.M.Cmd === OcpCmd.RD || io.ocp.M.Cmd === OcpCmd.WR) {
+    MAddrReg := io.ocp.M.Addr
+    MByteEnReg := io.ocp.M.ByteEn
+    armedReg := Bool(true)
+  }
+
+  when(io.ocp.M.Cmd === OcpCmd.WR) {
+    MDataReg := io.ocp.M.Data
+  }
+
   when(io.connectorSignals.enable === Bits(1)) {
 
-    when(io.ocp.M.Cmd === OcpCmd.RD || io.ocp.M.Cmd === OcpCmd.WR) {
+    when(armedReg || io.ocp.M.Cmd === OcpCmd.RD) {
       respReg := OcpResp.DVA
-      MAddrReg := io.ocp.M.Addr
+      armedReg := Bool(false)
     }
 
-    when(io.ocp.M.Cmd === OcpCmd.WR) {
+    when(armedReg || io.ocp.M.Cmd === OcpCmd.WR) {
+      respReg := OcpResp.DVA
       writeEnableReg := Bits(1)
-      MDataReg := io.ocp.M.Data
+      armedReg := Bool(false)
     }
   }
 
@@ -54,7 +67,7 @@ class SSPMConnector extends CoreDevice() {
 
   io.connectorSignals.M.Addr := MAddrReg
   io.connectorSignals.M.Data := MDataReg
-  io.connectorSignals.M.ByteEn := io.ocp.M.ByteEn
+  io.connectorSignals.M.ByteEn := MByteEnReg
   io.connectorSignals.M.WE := writeEnableReg
   io.ocp.S.Data := io.connectorSignals.S.Data
 }
@@ -74,7 +87,7 @@ object SSPMConnectorMain {
 class SSPMConnectorTester(dut: SSPMConnector) extends Tester(dut) {
 
   // Write test
-  //step(1)
+  step(1)
 
   poke(dut.io.ocp.M.Cmd, OcpCmd.WR.litValue())
   poke(dut.io.ocp.M.Addr, 1)
@@ -85,12 +98,20 @@ class SSPMConnectorTester(dut: SSPMConnector) extends Tester(dut) {
 
   step(1)
 
-  expect(dut.io.connectorSignals.M.ByteEn, Bits("b1111").litValue())
-  expect(dut.io.connectorSignals.M.Addr, 1)
+  poke(dut.io.ocp.M.Cmd, OcpCmd.IDLE.litValue())
+  poke(dut.io.ocp.M.Addr, 0)
+  poke(dut.io.ocp.M.Data, 0)
+  poke(dut.io.ocp.M.ByteEn, Bits("b0000").litValue())
 
+  expect(dut.io.connectorSignals.M.Addr, 1)
   expect(dut.io.connectorSignals.M.Data, 42)
+  expect(dut.io.connectorSignals.M.ByteEn, Bits("b1111").litValue())
 
   expect(dut.io.ocp.S.Resp, 1)
+
+  step(1)
+
+  peek(dut.io.ocp.S.Resp)
 
   // Read test
 
