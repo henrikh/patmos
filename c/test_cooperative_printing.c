01b6a7476d95bc4e32f57c4ffdb861abdee28ec8
Emad <s123791@student.dtu.dk>
2017-07-21 10:11:02 +0200

    Created cooperative printing test file.

--- c/test_cooperative_printing.c
+++ c/test_cooperative_printing.c
@@ -5,22 +5,12 @@
 #include "libcorethread/corethread.c"
 #include "libsspm/string_to_master.h"
 
-#define FLAG_ADDRESS 	(LOWEST_SPM_ADDRESS)
-#define LOCK_ADDRESS 	(LOWEST_SPM_ADDRESS + 4)
-#define CONTEST_ADDRESS 	(LOWEST_SPM_ADDRESS + 8)
-
-volatile _SPM int const *flag = FLAG_ADDRESS;
-volatile _SPM int const *lock = LOCK_ADDRESS;
-volatile _SPM int const *contest = CONTEST_ADDRESS;
-
-
 void slave(void* arg){
 	int cpuid = get_cpuid();
 	
 	if(cpuid == 1){
 		int i = 0;
 		send_from_core(1, "|core 1:0|\n");
-		///*
 		while(i<2){
 			if(can_send_from(1)){
 				if(i==0){
@@ -32,8 +22,34 @@ void slave(void* arg){
 				i++;
 			}
 		}
-		//*/
-		
+	}else if(cpuid == 2){
+		int i = 0;
+		send_from_core(2, "|core 2:0|\n");
+		while(i<2){
+			if(can_send_from(2)){
+				if(i==0){
+					send_from_core(2, "|core 2:1|\n");
+				}
+				if(i == 1){
+					send_from_core(2, "|core 2:2|\n");
+				}
+				i++;
+			}
+		}
+	}else if(cpuid == 3){
+		int i = 0;
+		send_from_core(3, "|core 3:0|\n");
+		while(i<2){
+			if(can_send_from(3)){
+				if(i==0){
+					send_from_core(3, "|core 3:1|\n");
+				}
+				if(i == 1){
+					send_from_core(3, "|core 3:2|\n");
+				}
+				i++;
+			}
+		}
 	}	
 }
 
@@ -49,12 +65,31 @@ int main()
 		corethread_create(&i, &slave, NULL);
 	}
 	///*
-	i = 0;
-	while((i<3)){
-		if(can_receive_from_core(1)){
+	int core1 = 0, 
+		core2 = 0,
+		core3 = 0;
+
+	while((core1<3) && (core2<3) && (core3<3)){
+		if(core1<3 && can_receive_from_core(1)){
+			printf("Core 1:");
 			printf(receive_from_core(1));
+			printf("\n");
 			free_received_from_core(1);
-			i++;
+			core1++;
+		}
+		if(core2<3 && can_receive_from_core(2)){
+			printf("Core 2:");			
+			printf(receive_from_core(2));
+			printf("\n");
+			free_received_from_core(2);
+			core2++;
+		}
+		if(core3<3 && can_receive_from_core(3)){
+			printf("Core 3:");			
+			printf(receive_from_core(3));
+			printf("\n");
+			free_received_from_core(3);
+			core3++;
 		}
 	}
 	//*/
@@ -62,6 +97,8 @@ int main()
 	for(i = 1; i < NR_CORES; i++){
 		corethread_join(i, (void **) &res);
 	}
+
+	printf("All cores done!\n");
 	/*
 	printf("Can receive: %x\n", can_receive_from_core(1));
 	printf("Received: '");
@@ -72,7 +109,6 @@ int main()
 	free_received_from_core(1);
 	printf("Can receive: %x\n", can_receive_from_core(1));
 	*/
-	
 }
 
 
