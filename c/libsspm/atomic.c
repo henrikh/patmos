c6098e8eaacb68a853f0910b11fab566eca01de2
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2017-07-31 11:59:16 +0200

    SSPM: Clean up of locking

--- c/libsspm/atomic.c
+++ c/libsspm/atomic.c
@@ -2,60 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include "atomic.h"
-#include "led.h"
+#include <machine/exceptions.h>
 
 
-int try_lock( volatile _SPM lock_t *l){
+int inline try_lock( volatile _SPM lock_t *l){
 	
 	int syncAddr = SCHEDULE_SYNC;
-	int s0;
-	int intWasOn;
 	lock_t lock_value;
 	
-	//Get register s0 value
+	intr_enable();
 	asm volatile(
-					"mfs %0 = $s0;"
-					: "=r" (s0)
-					: 
-					:
-	);
-	
-	//If interrupts are enabled, disable them
-	intWasOn = s0 % 2;
-	if(intWasOn){
-		s0++;
-		asm volatile(
-					"mts $s0 = %0;"
-					: 
-					: "r" (s0)
-					: "$s0"
-		);
-	}
-	
-	asm volatile(	"lwl $r0 = [%[sync]];" 	// Sync to TDMA
+				"lwl $r0 = [%[sync]];" 	// Sync to TDMA
 				"lwl %[lock_value] = [%[lock]];"	// Load lock value
 				"swl [%[lock]] = %[LOCKED];"	// write lock = LOCKED
 				: [lock_value] "=r" (lock_value), [lock] "+rm" (l)
 				: [sync] "r" (syncAddr), [LOCKED] "r" (LOCKED)
-				:
+				: "$r0"
 	);
-
-	//If interrupts were enabled before then enable them
-	if(intWasOn){
-		s0--;
-		asm volatile(
-					"mts $s0 = %0;"
-					: 
-					: "r" (s0)
-					: "$s0"
-		);
-	}
+	intr_disable();
 
 	return  lock_value == OPEN ;
 }
 
-void lock(volatile _SPM lock_t *l){
-	while( !try_lock(l)){}
+void inline lock(volatile _SPM lock_t *l){
+	while( !try_lock(l) ){}
 }
 
 void release( volatile _SPM lock_t *l ){
